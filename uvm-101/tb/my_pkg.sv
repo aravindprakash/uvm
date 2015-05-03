40d0a0759bd322eb4be3aaef8cc13dd9ff398e46
admin <info@chipverify.com>
2015-05-02 19:54:21 -0500

    Update environment uvm_info
    my_test_lib  - redundant file removed
    my_pkg - removed #100, objections

--- uvm-101/tb/my_pkg.sv
+++ uvm-101/tb/my_pkg.sv
@@ -26,13 +26,9 @@ package my_pkg;
       endfunction : build_phase
    
       task run_phase (uvm_phase phase);
-         phase.raise_objection (this);
          set_report_verbosity_level (UVM_MEDIUM);
-         uvm_report_info      (get_name(), $sformatf ("Hello UVM ! Simulation has started."), `__FILE__, `__LINE__);
-        
-         #100
+         uvm_report_info      (get_name(), $sformatf ("Hello UVM ! Simulation has started."), UVM_MEDIUM, `__FILE__, `__LINE__);
          `uvm_info   (get_name(), $sformatf("Finishing up with run_phase ... "), UVM_LOW)
-         phase.drop_objection (this);
       endtask : run_phase
    
    endclass : my_env
